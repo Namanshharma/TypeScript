b2c46481319dd5a2a099c263a8ffceb8c9a7e427
Naman Sharma <namansharma812@gmail.com>
2024-12-17 06:21:32 +0530

    basic questions

---
+++ Practice.ts
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------------------------ Loops --------------------------------------------------------------------------------------------------------------
+
+
+// Print number from 1- 10
+// for (let i = 0; i <= 10; i++) { console.log(i); }
+
+// Print the sum of first N natural numbers
+// let x = 0;
+// for (let i = 0; i <= 10; i++) { console.log(x += i) }
+
+// print square pattern with stars
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 0; j < 5; j++) {
+//         process.stdout.write("*"); // Print without newline
+//     }
+//     process.stdout.write("\n");
+// }
+
+// print the reverse of a given number
+// let x = 1023356353;
+// while (x > 0) {
+//     let z = x % 10;
+//     process.stdout.write(z.toString());
+//     x = Math.floor(x / 10);
+// }
+
+// print the reverse of originai number
+// let x = 134452782;
+// let reverse = 0;
+// while (x > 0) {
+//     let lastDigit = x % 10;
+//     reverse = (reverse * 10) + lastDigit;                // this is the main logic
+//     x = Math.floor(x / 10);
+// }
+// console.log(reverse);
+
+// check if number is prime or not
