d480c39ae9d2bf8027a0eaac97c506a2ef7e1488
Naman Sharma <namansharma812@gmail.com>
2024-12-19 22:59:30 +0530

    patterns commit

--- Practice.ts
+++ Practice.ts
@@ -35,3 +35,52 @@
 // console.log(reverse);
 
 // check if number is prime or not
+// const x: number = 2;
+// const checkPrime = (x: number): string => {
+//     for (let i = 2; i <= Math.sqrt(x); i++) {
+//         if (x % i == 0) {
+//             return "Not a Prime";
+//         }
+//     };
+//     return "Prime";
+// }
+// x == 2 ? console.log("Prime number") : console.log(checkPrime(x));
+
+// Pattern :- right angle triangle
+// const n = 4;
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         process.stdout.write("*");
+//     }
+//     console.log("");
+// }
+
+// Pattern :- Inverted Right angle triangle
+// const n = 4;
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n - i; j++) {
+//         process.stdout.write("*");
+//     }
+//     console.log("");
+// }
+
+// Pattern :- right angle tiangle with numbers
+// const n = 4;
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         process.stdout.write((j + 1).toString());
+//     }
+//     console.log();
+// }
+
+// pattern :- hollow square
+const n = 5;
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+            process.stdout.write("*");
+        }
+        else { process.stdout.write("") };
+    }
+    console.log();
+}
