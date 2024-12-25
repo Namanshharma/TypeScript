d7f03e7dba1286002dc274369ec4af1059e28a79
Naman Sharma <namansharma812@gmail.com>
2024-12-26 00:21:20 +0530

    Butterfly pattern

--- Practice.ts
+++ Practice.ts
@@ -118,3 +118,27 @@
 //     }
 //     console.log();
 // }
+
+// pattern 0-1 triangle
+// const n = 5;
+// for (let i = 0; i <= n; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         (i + j) % 2 == 0 ? process.stdout.write("1") : process.stdout.write("0");
+//     }
+//     console.log();
+// }
+
+// pattern Butterfly
+// const n = 5;
+// for (let i = 0; i <= n; i++) {
+//     for (let j = 0; j <= i; j++) process.stdout.write("*");
+//     for (let j = 0; j <= 2 * (n - i); j++) process.stdout.write(" ");
+//     for (let j = 0; j <= i; j++) process.stdout.write("*");
+//     console.log();
+// }
+// for (let i = n; i >= 0; i--) {
+//     for (let j = 0; j <= i; j++) process.stdout.write("*");
+//     for (let j = 0; j <= 2 * (n - i); j++) process.stdout.write(" ");
+//     for (let j = 0; j <= i; j++) process.stdout.write("*");
+//     console.log();
+// }
