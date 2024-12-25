ea8e258478784cf0cae57347a7c214bec394a87a
Naman Sharma <namansharma812@gmail.com>
2024-12-26 01:34:45 +0530

    Diamond pattern and diamond questions finished

--- Practice.ts
+++ Practice.ts
@@ -142,3 +142,36 @@
 //     for (let j = 0; j <= i; j++) process.stdout.write("*");
 //     console.log();
 // }
+
+// pattern Solid Rhombus
+// const n = 5;
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n - i - 1; j++) process.stdout.write(" ");
+//     for (let j = 0; j <= n; j++) process.stdout.write("*");
+//     console.log();
+// }
+
+// pattern hollow rhombus
+// const n = 5;
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n - i - 1; j++) process.stdout.write(" ");
+//     for (let j = 0; j < n; j++) {
+//         if (j == 0 || j == n - 1 || i == n - 1 || i == 0) process.stdout.write("*");
+//         else process.stdout.write(" ");
+//     }
+//     console.log();
+// }
+
+// Diamond patter
+// const n = 5;
+// for (let i = 0; i <= n; i++) {
+//     for (let j = 0; j <= n - i - 1; j++) process.stdout.write(" ");
+//     for (let j = 0; j < (2 * i) + 1; j++) process.stdout.write("*");
+//     console.log();
+// }
+// for (let i = n; i >= 0; i--) {
+//     for (let j = 0; j <= n - i - 1; j++) process.stdout.write(" ");
+//     for (let j = 0; j < (2 * i) + 1; j++) process.stdout.write("*");
+//     console.log();
+// }
+
